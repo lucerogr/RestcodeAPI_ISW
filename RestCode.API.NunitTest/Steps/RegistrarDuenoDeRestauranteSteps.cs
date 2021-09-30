9c95fa01961a32abf89402925c3beae9a0a75f47
renato-17 <renato.avela.17@gmail.com>
2021-09-30 14:24:07 -0500

    testing updated

---
+++ RestCode.API.NunitTest/Steps/RegistrarDuenoDeRestauranteSteps.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using RestCode_WebApplication.Domain.Models;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace RestCode.API.SpecTest.Steps
+{
+    [Binding]
+    public class RegistrarDuenoDeRestauranteSteps
+    {
+        public static RestClient restClient;
+        public static RestRequest restRequest;
+        public static IRestResponse response;
+        private static Owner owner;
+
+        [Given(@"que el dueño de restaurante se encuentra en la pantalla de crear una cuenta")]
+        public void GivenQueElDuenoDeRestauranteSeEncuentraEnLaPantallaDeCrearUnaCuenta()
+        {
+            restClient = new RestClient("https://localhost:44316/");
+            restRequest = new RestRequest("api/owners", Method.POST);
+            restRequest.RequestFormat = DataFormat.Json;
+        }
+        
+        [When(@"ingresa sus datos personales")]
+        public void WhenIngresaSusDatosPersonales(Table table)
+        {
+            owner = table.CreateInstance<Owner>();
+            owner = new Owner()
+            {
+                UserName = "cintia12",
+                FirstName = "Cintia",
+                LastName = "Rosales",
+                Cellphone = "912334411",
+                Email = "cintiar@gmail.com",
+                Password = "rosalescintia",
+                Ruc = 12745621351
+            };
+            restRequest.AddJsonBody(owner);
+            response = restClient.Execute(restRequest);
+        }
+        
+        [Then(@"el sistema guarda todos los datos registrados por el usuario")]
+        public void ThenElSistemaGuardaTodosLosDatosRegistradosPorElUsuario()
+        {
+            Assert.That(12745621351, Is.EqualTo(owner.Ruc));
+        }
+    }
+}
---
+++ RestCode.API.NunitTest/Steps/RegistrarVentasDeDuenoDeRestauranteSteps.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using RestCode_WebApplication.Domain.Models;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace RestCode.API.SpecTest.Steps
+{
+    [Binding]
+    public class RegistrarVentasDeDuenoDeRestauranteSteps
+    {
+        public static RestClient restClient;
+        public static RestRequest restRequest;
+        public static IRestResponse response;
+        private static Sale sale;
+
+        [Given(@"que el dueño del restaurante se encuentra en la sección de Registros/Añadir")]
+        public void GivenQueElDuenoDelRestauranteSeEncuentraEnLaSeccionDeRegistrosAnadir()
+        {
+            restClient = new RestClient("https://localhost:44316/");
+            restRequest = new RestRequest("api/sales", Method.POST);
+            restRequest.RequestFormat = DataFormat.Json;
+        }
+        
+        [When(@"termina de colocar los datos de su venta")]
+        public void WhenTerminaDeColocarLosDatosDeSuVenta(Table table)
+        {
+            sale = table.CreateInstance<Sale>();
+            sale = new Sale()
+            {
+                DateAndTime = DateTime.Parse("2021/05/29"),
+                ClientFullName = "Gloria Prado",
+                RestaurantId = 1
+            };
+            restRequest.AddJsonBody(sale);
+            response = restClient.Execute(restRequest);
+        }
+        
+        [Then(@"el sistema guarda la venta exitosamente")]
+        public void ThenElSistemaGuardaLaVentaExitosamente()
+        {
+            Assert.That("Gloria Prado", Is.EqualTo(sale.ClientFullName));
+        }
+    }
+}
